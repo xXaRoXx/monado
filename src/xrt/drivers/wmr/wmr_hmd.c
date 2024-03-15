5758066381d6af1ddabbe1facb370afcd67fb074
Jan Schmidt <jan@centricular.com>
2024-03-16 01:20:24 +1100

    d/wmr: Fix code format

--- src/xrt/drivers/wmr/wmr_hmd.c
+++ src/xrt/drivers/wmr/wmr_hmd.c
@@ -1252,7 +1252,9 @@ wmr_hmd_destroy(struct xrt_device *xdev)
 XRT_MAYBE_UNUSED static struct t_camera_calibration
 wmr_hmd_get_cam_calib(struct wmr_hmd *wh, int cam_index)
 {
-	struct t_camera_calibration res = { 0, };
+	struct t_camera_calibration res = {
+	    0,
+	};
 	struct wmr_camera_config *wcalib = wh->config.tcams[cam_index];
 	struct wmr_distortion_6KT *intr = &wcalib->distortion6KT;
 
